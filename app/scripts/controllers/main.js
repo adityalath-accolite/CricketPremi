a8b53cd9dcc9e26a2c493d7b9d5cba11ebdba824
Aditya Lath <aditya.lath@accolitedigital.com>
2021-02-19 18:59:56 +0530

    removed unwanted code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,8 @@
  * Controller of the viaGruntApp
  */
 angular.module('viaGruntApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
